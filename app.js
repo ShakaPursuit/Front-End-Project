fda205f211b0312bae1dfc9b95a9c9e852a57ec0
ShakaPursuit <shakalivngstone@pursuit.org>
2023-07-11 12:03:31 -0400

    found Rick and Morty api working accessing data in dom

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-fetch("https://cors-anywhere.herokuapp.com/api.thesneakerdatabase.com") // see actual random user
-.then((response) => response.json())
-.then((JSONresponse) => console.log(JSONresponse))
 
-//   .catch( (err) => displayError(err) );
+
+        // fetch('https://rickandmortyapi.com/api/location')
+        // .then(response => response.json())
+        // .then(json => console.log(json))
+
+
+
+
+        // fetch('https://rickandmortyapi.com/api/episode')
+        // .then(response => response.json())
+        // .then(json => console.log(json))
+
+        const myDiv=document.querySelector('div')
+
+
+        function chooseCharacter({results}){
+
+            const [myCharacter]=results;
+
+        //   myDiv.innerHTML=myCharacter
+        console.log("here are my result :", myCharacter)
+
+        console.log("here are my resultImage :", myCharacter.image)
+
+        console.log("here are my result :", myCharacter.url)
+
+
+        console.log("here are my result :", myCharacter.url)
+
+        console.log("here are my result :", myCharacter.url)
+
+        console.log("here are my result :", myCharacter.url)
+
+
+        console.log("here are my result :", myCharacter.url)
+
+        console.log("here are my result :", myCharacter.url)
+
+        console.log("here are my result :", myCharacter.url)
+
+        }
+        fetch('https://rickandmortyapi.com/api/character/?page=20')
+        .then(response => response.json())
+        .then(json => chooseCharacter(json))
+        .catch(chooseCharacter);
+
 
