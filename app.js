4b23ca649e2f3e9d3ff5d8e79bd15b51d17e89be
ShakaPursuit <shakalivngstone@pursuit.org>
2023-07-11 23:54:37 -0400

    basic layout ainteracting with api

--- app.js
+++ app.js
@@ -11,9 +11,9 @@
         // .then(response => response.json())
         // .then(json => console.log(json))
 
-        const myDiv=document.querySelector('div')
+       
 
-
+//Character 1
         function chooseCharacter({results}){
 
             const [myCharacter]=results;
@@ -23,26 +23,71 @@
 
         console.log("here are my resultImage :", myCharacter.image)
 
-        console.log("here are my result :", myCharacter.url)
+        console.log("here are my episode Link :", myCharacter.episode)
 
 
-        console.log("here are my result :", myCharacter.url)
+        console.log("here are my result :", myCharacter.id)
 
-        console.log("here are my result :", myCharacter.url)
+        console.log("here are my result Name :", myCharacter.name)
 
-        console.log("here are my result :", myCharacter.url)
+        console.log("here are my result species Type :", myCharacter.species)
 
 
-        console.log("here are my result :", myCharacter.url)
+        console.log("here are my result :", myCharacter.status)
 
         console.log("here are my result :", myCharacter.url)
 
         console.log("here are my result :", myCharacter.url)
 
         }
+        // fetch('https://rickandmortyapi.com/api/character/?page=20')
+        // .then(response => response.json())
+        // .then(json => chooseCharacter(json))
+        // .catch(chooseCharacter);
+
+
+    //    const results= fetch('https://rickandmortyapi.com/api/character/?page=20')
+    //     .then(response => response.json())
+    //     .then(json => {const results= console.log(json)
+        
+    //     console.log("Here are my new results:",results)
+    //     })
+       
         fetch('https://rickandmortyapi.com/api/character/?page=20')
         .then(response => response.json())
-        .then(json => chooseCharacter(json))
-        .catch(chooseCharacter);
+        .then(json => {const results= json.results;
+        
+        console.log("Here are my new results:",results)
+       
+        console.log(results[0].image)
+
+        const choosePic= document.querySelector('.pic')
+        const choosePic2= document.querySelector('.pic2')
+
+        const choosePic3= document.querySelector('.pic3')
 
 
+        choosePic.src =results[2].image
+        choosePic2.src =results[19].image
+        choosePic3.src =results[11].image
+
+
+        console.log(results[1])
+
+
+        console.log(results[2])
+
+
+        console.log(results[3])
+
+
+        console.log(results[4])
+
+
+        console.log(results[5])
+
+    
+    })
+       
+
+       
